b234b062c99ccff1a85007b42eafd585a3c7ea37
“Daisy <daisyding666@gmail.com>
2024-08-02 14:57:43 -0700

    lab5

---
+++ src/Labs/Lab5/index.tsx
@@ -0,0 +1,32 @@
+import EnvironmentVariables from "./EnvironmentVariables";
+import PathParameters from "./PathParameters";
+import QueryParameters from "./QueryParameters";
+import WorkingWithArrays from "./WorkingWithArrays";
+import WorkingWithObjects from "./WorkingWithObjects";
+import HttpClient from "./HttpClient";
+const REMOTE_SERVER = process.env.REACT_APP_REMOTE_SERVER;
+
+export default function Lab5() {
+  return (
+    <div id="wd-lab5" className="container-fluid">
+      <h2>Lab 5</h2>
+      <div id="wd-github">
+        <a href="https://github.com/daisydbbb/kanbas-react-web-app/tree/a5">
+          GitHub
+        </a>
+      </div>
+      <div className="list-group">
+        <a href={`${REMOTE_SERVER}/lab5/welcome`} className="list-group-item">
+          Welcome
+        </a>
+      </div>
+      <hr />
+      <EnvironmentVariables />
+      <PathParameters />
+      <QueryParameters />
+      <WorkingWithObjects />
+      <WorkingWithArrays />
+      <HttpClient />
+    </div>
+  );
+}
